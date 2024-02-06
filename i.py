aea33a1fc471324633b1ed852a396bfac31bdfab
Mahin Ahmed <106468659+FB-KING@users.noreply.github.com>
2024-02-07 04:24:23 +0600

    Add files via upload

--- i.py
+++ i.py
@@ -1,14 +1,17 @@
 import os,sys
 try:
-        import os, sys, rich, requests
-       # mkdir('/sdcard/xxsvr.txt')
-      #  checkhigh = '/sdcard/xxsvr.txt'
-     #   if 'permission denied' in checkhigh:
-        	#print(f'First Give Storage Permission..');exit()
+	import os, sys, rich, requests
+	try:
+		mkdir('/sdcard/xxsvr.txt')
+	except:print('sex');exit()
+	checkhigh = '/sdcard/xxsvr.txt'
+	if 'permission denied' in checkhigh:
+		print(f'First Give Storage Permission..');exit()
 except PermissionError:
-        os.system('termux-setup-storage')
-        os.system('git pull')
-        
+	os.system('termux-setup-storage')
+	#os.system('termux-setup-storage')
+      #  os.system('git pull')
+	
 def let():
 	print('xxxx');exit()
 	
