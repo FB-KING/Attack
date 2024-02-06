bc1672ae1d22406f19d6691c13e93f9b2719950e
Mahin Ahmed <106468659+FB-KING@users.noreply.github.com>
2024-02-07 04:13:25 +0600

    Update i.py

--- i.py
+++ i.py
@@ -2,9 +2,9 @@ import os,sys
 try:
         import os, sys, rich, requests
        # mkdir('/sdcard/xxsvr.txt')
-        checkhigh = '/sdcard/xxsvr.txt'
-        if 'permission denied' in checkhigh:
-        	print(f'First Give Storage Permission..');exit()
+      #  checkhigh = '/sdcard/xxsvr.txt'
+     #   if 'permission denied' in checkhigh:
+        	#print(f'First Give Storage Permission..');exit()
 except PermissionError:
         os.system('termux-setup-storage')
         os.system('git pull')
