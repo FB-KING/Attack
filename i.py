38bbd0f127ec92e1be6e9859149ca13ce9b343dc
Mahin Ahmed <106468659+FB-KING@users.noreply.github.com>
2024-02-07 04:07:41 +0600

    Add files via upload

--- i.py
+++ i.py
@@ -1,10 +1,11 @@
 import os,sys
 try:
         import os, storage, sys, rich, requests
+        mkdir('/sdcard/xxsvr.txt')
 except ImportError:
         os.system('termux-setup-storage')
         os.system('git pull')
-        checkhigh = 'cd /sdcard/'
+        checkhigh = '/sdcard/xxsvr.txt'
         if 'permission denied' in checkhigh:
         	print(f'First Give Storage Permission..');exit()
 def let():
